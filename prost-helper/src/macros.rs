fde2d8f185c1fe047f92a1eb39ee4803d52f917b
Tyr Chen <tyr.chen@gmail.com>
2021-02-21 22:43:03 -0800

    make macros support repetition

--- prost-helper/src/macros.rs
+++ prost-helper/src/macros.rs
@@ -4,8 +4,8 @@
 /// Note both `$type` and `&$type` will be generated.
 #[macro_export]
 macro_rules! prost_into_vec {
-    ($type:ty, $cap:expr) => {
-        impl std::convert::From<$type> for Vec<u8> {
+    ($(($type:ty, $cap:expr)),*) => {
+        $(impl std::convert::From<$type> for Vec<u8> {
             fn from(msg: $type) -> Self {
                 let mut buf = bytes::BytesMut::with_capacity($cap);
                 msg.encode(&mut buf).unwrap();
@@ -18,7 +18,7 @@ macro_rules! prost_into_vec {
                 msg.encode(&mut buf).unwrap();
                 buf.to_vec()
             }
-        }
+        })*
     };
 }
 
@@ -28,8 +28,8 @@ macro_rules! prost_into_vec {
 /// Please remember to `use std::convert::TryInto;` when you want to use generated functions.
 #[macro_export]
 macro_rules! vec_try_into_prost {
-    ($type:ty) => {
-        impl std::convert::TryFrom<Vec<u8>> for $type {
+    ($($type:ty),*) => {
+        $(impl std::convert::TryFrom<Vec<u8>> for $type {
             type Error = prost::DecodeError;
             fn try_from(buf: Vec<u8>) -> Result<Self, Self::Error> {
                 let msg: $type = Message::decode(&buf[..])?;
@@ -43,19 +43,20 @@ macro_rules! vec_try_into_prost {
                 let msg: $type = Message::decode(buf)?;
                 Ok(msg)
             }
-        }
+        })*
     };
 }
 
 #[cfg(feature = "json")]
 #[macro_export]
 macro_rules! prost_to_json {
-    ($type:ty) => {
-        impl crate::ToJson for $type {
+
+    ($($type:ty),*) => {
+        $(impl crate::ToJson for $type {
             fn to_json(&self) -> String {
                 serde_json::to_string(self).unwrap()
             }
-        }
+        })*
     };
 }
 
@@ -73,10 +74,16 @@ mod tests {
         pub value: u64,
     }
 
+    #[derive(Clone, PartialEq, Message, Serialize, Deserialize)]
+    struct World {
+        #[prost(string, tag = "1")]
+        pub msg: String,
+    }
+
     #[test]
     fn test_prost_try_into_vec() {
-        prost_into_vec!(Hello, 32);
-        vec_try_into_prost!(Hello);
+        prost_into_vec!((Hello, 32), (World, 256));
+        vec_try_into_prost!(Hello, World);
         let hello = Hello::default();
         let data: Vec<u8> = hello.clone().into();
         let data_ref = &data[..];
@@ -93,7 +100,8 @@ mod tests {
     #[test]
     fn test_prost_to_json() {
         use crate::ToJson;
-        prost_to_json!(Hello);
+
+        prost_to_json!(Hello, World);
         let hello = Hello::default();
         let result = hello.to_json();
         assert_eq!(result, r#"{"msg":"","value":0}"#);
