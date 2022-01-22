394fd071b9728c655895432ec62b3e164e4b5ef2
tyrchen <tyrchen@users.noreply.github.com>
2022-01-22 05:58:53 +0000

    deploy: 86ae9d5d89f23959f59a8a0dc759981f31f8d9c9

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["prost_build_config"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"prost_build_config/struct.BuildConfig.html\" title=\"struct prost_build_config::BuildConfig\">BuildConfig</a>&gt; for <a class=\"struct\" href=\"prost_build_config/struct.Builder.html\" title=\"struct prost_build_config::Builder\">Builder</a>","synthetic":false,"types":["prost_build_config::Builder"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
