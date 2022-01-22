8740ec72d9016fa5b2f9c5bd01ff26fa49470c17
Tyr Chen <tyr.chen@gmail.com>
2022-01-21 20:26:38 -0800

    rename prost-serde to prost-build-config and change the config to yaml format to simplify it.

--- prost-build-config/examples/pb/helloworld.rs
+++ prost-build-config/examples/pb/helloworld.rs
@@ -1,18 +1,17 @@
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, validator::Validate)]
 #[serde(default)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Hello {
     #[prost(string, tag="1")]
     #[serde(skip_serializing_if = "String::is_empty", default)]
+#[validate(email)]
     pub msg: ::prost::alloc::string::String,
     #[prost(string, tag="2")]
     #[serde(deserialize_with = "prost_helper::deserialize_null_default")]
     pub field_may_be_null: ::prost::alloc::string::String,
     #[prost(uint64, tag="3")]
-    #[serde(skip_serializing_if = "prost_helper::is_zero")]
     pub field_skip_zero: u64,
     #[prost(string, tag="4")]
-    #[serde(skip_serializing)]
     pub filed_skip: ::prost::alloc::string::String,
     #[prost(bytes="bytes", tag="5")]
     #[serde(serialize_with = "prost_helper::serialize_buf",deserialize_with = "prost_helper::deserialize_buf_bytes")]
@@ -24,10 +23,20 @@ pub struct Hello {
     #[serde(serialize_with = "prost_helper::serialize_buf",deserialize_with = "prost_helper::deserialize_buf_bytes")]
     pub map: ::prost::alloc::collections::BTreeMap<::prost::alloc::string::String, ::prost::bytes::Bytes>,
     #[prost(bytes="bytes", repeated, tag="8")]
-    #[serde(serialize_with = "prost_helper::serialize_repeat_buf",deserialize_with = "prost_helper::deserialize_repeat_buf_bytes")]
+    #[serde(serialize_with = "prost_helper::serialize_repeat_buf", deserialize_with = "prost_helper::deserialize_repeat_buf_bytes")]
     pub list_data: ::prost::alloc::vec::Vec<::prost::bytes::Bytes>,
+    #[prost(message, optional, tag="9")]
+    #[validate(required)]
+    pub world: ::core::option::Option<World>,
 }
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, validator::Validate)]
+#[serde(default)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct World {
+    #[prost(uint32, tag="1")]
+    pub world: u32,
+}
+#[derive(serde::Serialize, serde::Deserialize, validator::Validate)]
 #[serde(rename_all = "lowercase")]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
---
+++ prost-build-config/src/lib.rs
@@ -0,0 +1,172 @@
+//! A prost toolkit to build protobuf with extra derive/attributes support.
+//!
+//! Usually when we define our protobuf messages, we hope some of the generated
+//! data structures can derive other traits. e.g. serde support. Fortunately
+//! `prost-build` has that capability - you can create a config with
+//! `prost_build::Config::new()` and then set proper attributes for type or field.
+//! For exmaple, you can add serde support for this structure by using:
+//!
+//! ```ignore
+//! config.type_attribute("package.RequestPing", "#[derive(serde::Serialize, serde::Deserialize)]");
+//! config.type_attribute("package.RequestPing", "#[serde(default)]");
+//! ```
+//!
+//! and you will get this generated code:
+//!
+//! ```ignore
+//! #[derive(serde::Serialize, serde::Deserialize)]
+//! #[serde(default)]
+//! #[derive(Clone, PartialEq, ::prost::Message)]
+//! pub struct RequestPing {
+//!     #[prost(string, tag = "1")]
+//!     pub ping: ::prost::alloc::string::String,
+//! }
+//! ```
+//!
+//! This crate helps to simplify this build script by using a predefined build configuration.
+//!
+//! # Getting started
+//!
+//! First of all, you shall create a YAML file which contains configuration for the builder. You can
+//! get a copy of a default YAML file from: [default_build_config.yml](https://raw.githubusercontent.com/tyrchen/prost-helper/master/prost-build-config/default_build_config.yml). See an example of [build_config.yml](https://raw.githubusercontent.com/tyrchen/prost-helper/master/prost-build-config/examples/build_config.yml).
+//! Please add the proto files, proto includes, output dir, and the data structure or field you want
+//! to add the desired attributes.
+//! Then you could use it in:
+//!
+//! ```ignore
+//! use prost_build_config::Builder;
+//!
+//! fn main() {
+//!     let content = include_str!("../examples/build_config.json");
+//!     let builder: BuildConfig = serde_yaml::from_str(content).unwrap();
+//!     builder.build_protos();
+//! }
+//! ```
+
+use prost_build::Config;
+use serde::{Deserialize, Serialize};
+use std::{fs, process::Command};
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+#[serde(default)]
+pub struct BuildConfig {
+    /// protobuf include dirs
+    pub includes: Vec<String>,
+    /// protobuf files
+    pub files: Vec<String>,
+    /// dir for generated code, defaults to Cargo OUT_DIR, else the current dir
+    pub output: Option<String>,
+    /// build options for messages
+    pub messages: Vec<BuildOption>,
+    /// build options for enums
+    pub enums: Vec<BuildOption>,
+    /// build options for fields
+    pub fields: Vec<BuildOption>,
+    /// build options for bytes
+    pub bytes: Vec<String>,
+    /// build options for BTreeMap
+    pub btree_maps: Vec<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+#[serde(default)]
+pub struct BuildOption {
+    /// a list of paths you want to add the attribute
+    pub paths: Vec<String>,
+    /// description of the option
+    pub description: String,
+    /// extra attributes to put on generated data structure, for example: `derive(Serialize, Deserialize)`
+    /// it will be converted to `#[derive(Serialize, Deserialize)]`
+    pub attrs: Vec<String>,
+}
+
+pub struct Builder {
+    /// prost configuration
+    config: Config,
+    /// protobuf include dirs
+    pub includes: Vec<String>,
+    /// protobuf files
+    pub files: Vec<String>,
+}
+
+impl Builder {
+    /// Initialize a Builder for building the protobuf files with the build opts provided by a BuildConfig
+    pub fn new(config: BuildConfig) -> Self {
+        // For the output directory, use the specified one, or fallback to the
+        // OUT_DIR env variable provided by Cargo if it exists (it should!), else
+        // fallback to the current directory.
+        let output_dir: String = match &config.output {
+            None => {
+                let default_output_dir = std::env::var("OUT_DIR");
+
+                match default_output_dir {
+                    Err(_) => String::new(),
+                    Ok(cargo_out_dir) => cargo_out_dir,
+                }
+            }
+            Some(specified_output) => specified_output.to_owned(),
+        };
+
+        let mut c = Config::new();
+
+        c.btree_map(config.btree_maps);
+        c.bytes(config.bytes);
+
+        for opt in config.messages {
+            for p in opt.paths {
+                c.type_attribute(p, to_attr(&opt.attrs));
+            }
+        }
+
+        for opt in config.enums {
+            for p in opt.paths {
+                c.type_attribute(p, to_attr(&opt.attrs));
+            }
+        }
+
+        for opt in config.fields {
+            for p in opt.paths {
+                c.field_attribute(p, to_attr(&opt.attrs));
+            }
+        }
+
+        fs::create_dir_all(&output_dir).unwrap();
+        c.out_dir(&output_dir);
+        Self {
+            config: c,
+            includes: config.includes,
+            files: config.files,
+        }
+    }
+
+    pub fn build_protos(&mut self) {
+        self.config
+            .compile_protos(&self.files, &self.includes)
+            .unwrap_or_else(|e| panic!("Failed to compile proto files. Err: {:?}", e));
+
+        Command::new("cargo")
+            .args(&["fmt"])
+            .status()
+            .expect("cargo fmt failed");
+    }
+}
+
+fn to_attr(attrs: &[String]) -> String {
+    attrs
+        .iter()
+        .map(|s| format!("#[{}]", s))
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn generate_serde_validator_supported_code() {
+        let content = include_str!("../examples/build_config.yml");
+        let config: BuildConfig = serde_yaml::from_str(content).unwrap();
+        let mut builder = Builder::new(config);
+        builder.build_protos();
+    }
+}
