4f9a1e94b95962ae684c03125b9005a67332ae88
tyrchen <tyrchen@users.noreply.github.com>
2023-12-18 15:06:52 +0000

    deploy: 81f91258324c5837916d3822fabe708d59638b19

--- crates.js
+++
@@ -1,1 +0,0 @@
-window.ALL_CRATES = ["prost_build_config","prost_helper"];
--- source-files.js
+++
@@ -1,4 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["prost_build_config"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["prost_helper"] = {"name":"","files":["buf.rs","lib.rs","macros.rs"]};
-createSourceSidebar();
